7b6a101cbb129513c8ca1d20ddc1361292619ee8
shubhamtyagi404 <shubhamtyagi404@gmail.com>
2019-01-28 22:31:05 +0530

    commit

---
+++ webapi/TopsApi/Controllers/ReprocessController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TopsApi.Business;
+
+namespace TopsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReprocessController : ControllerBase
+    {
+
+    }
+}
